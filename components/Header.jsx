54c807f460036099bb33200f9cd0f19f067aae2f
Russell Conard <russell@naturenumbers.com>
2021-11-16 17:10:14 +0000

    Convert Header to a pure functional component

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,37 +1,18 @@
 import React from 'react';
 import styles from '../styles/header.module.css';
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-    };
-  }
-
-  render() {
-    const {
-    } = this.state;
-
-    return (
-      <header
-        className={styles["header-layout"]} >
-        <div
-          className={styles["header-name"]} >
-          <h1>
-            Azure IoT Expense Estimator
-          </h1>
-        </div>
-      </header>
-    );
-  }
-}
-
-export async function getStaticProps(context) {
-  return {
-    props: {
-    },
-  };
+const Header = function () {
+  return (
+    <header
+      className={styles["header-layout"]} >
+      <div
+        className={styles["header-name"]} >
+        <h1>
+          Azure IoT Expense Estimator
+        </h1>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
